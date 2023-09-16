0f10d98d8463bf2c4b5a0ea2523584bd369acda6
mpark63 <minspk13@gmail.com>
2023-09-16 00:44:48 -0400

    connect socket to right port

--- src/socketio.service.js
+++ src/socketio.service.js
@@ -1,11 +1,12 @@
 import { io } from 'socket.io-client';
+import { getApiUrl } from './utils';
 
 class SocketioService {
   socket;
   positions = [];
 
   setupSocketConnection() {
-    this.socket = io('http://localhost:4567');
+    this.socket = io(getApiUrl());
 
     this.socket.on('connect', function () {
       console.log('connected to socket');
